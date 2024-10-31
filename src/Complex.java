ca9bc4d4f0743505184b992c9d55ed76b68dc226
GERMAN <fingel232@icloud.com>
2024-10-31 16:34:03 +0300

    Добавлено 3 первых пункта

--- src/Complex.java
+++ src/Complex.java
@@ -1,5 +1,28 @@
 public class Complex {
-    public static void main(String[] args) {
+    int x;
+    int y;
 
+    public Complex() {
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public Complex(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.io.FilterOutputStream;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Hello world!
+
     }
 }
